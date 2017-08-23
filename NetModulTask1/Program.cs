ec2d270af3ee43408d5584c13c65277f211dfe2d
taufikprabowo1121 <solidiercyber@gmail.com>
2017-08-23 16:40:19 +0700

    NetModuleTask1

---
+++ NetModulTask1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetModulTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alas;
+            int tinggi;
+            int hitung;
+            Console.Write("Masukan Alas :");
+            alas = int.Parse(Console.ReadLine());
+            Console.Write("Masukan Tinggi :");
+            tinggi = int.Parse(Console.ReadLine());
+            if (alas < 0 || tinggi < 0)
+            {
+                Console.WriteLine("Sorry must input positive number");
+            }
+            else
+            {
+                hitung = alas * tinggi / 2;
+                Console.WriteLine("Hasil Perhitungan :" + hitung);
+            }
+            Console.ReadKey();
+        }
+    }
+}
